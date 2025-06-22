dde371420cc29a96d6c17fe96422ab2e27286d8c
DekayDeCanela <diegoszm11@gmail.com>
2025-06-21 21:29:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
